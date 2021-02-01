34133c30d548a2e8ebf72b5189f6bfad441de5ca
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-01 22:21:58 +0600

    declare let and const

---
+++ letConst.js
@@ -0,0 +1,26 @@
+const name = 'abir';
+
+// console.log(name);
+
+const num = [12,34];
+num[1]=20;
+num.push(30);
+num.push('abir');
+const nayok = {
+    name:'karim',
+    age:24
+
+}
+
+// num = [5,6];
+// num = ['karim','abir'];
+// console.log(nayok);
+let pName = "karim";
+pName = 'akram';
+console.log(pName);
+
+let sum = 0;
+for(let i=0;i<5;i++){
+    sum = sum + i;
+    console.log(sum);
+}
