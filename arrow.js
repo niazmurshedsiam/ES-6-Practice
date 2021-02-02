a079c5244089deab68e14356dba218ffb0b443ca
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-02 11:13:49 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,28 @@
+// const doubleInt = function (num){
+//     return num * 2;
+
+// }
+// const doubleInt = num => num * 2;
+
+// const result = doubleInt(50);
+// console.log(result);
+
+// const add = (a,b) => a + b;
+// const give5= ()=>5;
+// const result1 = give5();
+// console.log(result1);
+
+// const result = add(5,6);
+// console.log(result);
+
+const doMath = (x,y) => {
+    const sum = x + y;
+    const dif = x -y
+    const result = sum * dif;
+    return result;
+
+
+}
+const result3 = doMath(15,10);
+console.log(result3);
+
---
+++ threedot.js
@@ -0,0 +1,7 @@
+const a = [2,3,5];
+const b = [3,55,7];
+console.log(...a,5,...b);
+
+const takaPoisa = [650,450,250,850];
+const maximum = Math.max(...takaPoisa);
+console.log(maximum);
