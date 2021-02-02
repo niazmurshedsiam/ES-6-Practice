b390758c10bed8640b4d2c0155a99bc4a2dd028f
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-02 10:28:44 +0600

    template string multiLine

---
+++ template.js
@@ -0,0 +1,8 @@
+const firstName = 'Justin';
+const lastName = 'Bibar';
+console.log(`${firstName} ${lastName} is a boy.`)
+
+const multiline = `I love u.
+Miss u.
+don't love u.`;
+console.log(multiline);
