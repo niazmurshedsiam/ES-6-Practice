01096b5911ddadd01638dff8824fcab4d5bc492e
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-02 11:21:10 +0600

    threedot

--- threedot.js
+++ threedot.js
@@ -2,6 +2,6 @@ const a = [2,3,5];
 const b = [3,55,7];
 console.log(...a,5,...b);
 
-const takaPoisa = [650,450,250,850];
+const takaPoisa = [650,450,250,850,359];
 const maximum = Math.max(...takaPoisa);
 console.log(maximum);
